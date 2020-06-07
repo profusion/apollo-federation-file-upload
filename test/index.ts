8a6920b391c4b7d2baf6e9243f107b691e35779f
Guilherme Iscaro <cabelitostos@gmail.com>
2020-06-07 18:43:42 -0300

    feat: add testing env
    This commit the support for testing the file upload support in action.
    The main object is to create a graphql API which accepts file as input
    and returns the file content as a response. This is preferred over jest
    testing, since we can't fully test connection between apollo gateway
    and the micro service using jest.

---
+++ test/index.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-await-in-loop */
+import newman, { NewmanRunSummary } from 'newman';
+
+import StartGateway from './gateway';
+import StartDownloadService from './download-service';
+
+// eslint-disable-next-line import/extensions
+import collection from './collection.json';
+
+const { GATEWAY_PORT = '4000', MAX_CONNECT_RETRIES = '5' } = process.env;
+
+const maxRetries = parseInt(MAX_CONNECT_RETRIES, 10);
+
+const sleep = (milliseconds: number): Promise<void> =>
+  new Promise(resolve => setTimeout(resolve, milliseconds));
+
+const runTests = (): Promise<void> =>
+  new Promise((resolve, reject) => {
+    newman.run(
+      {
+        collection,
+        globals: {
+          id: '5bfde907-2a1e-8c5a-2246-4aff74b74236',
+          name: 'test-env',
+          values: [
+            {
+              key: 'PORT',
+              type: 'text',
+              value: GATEWAY_PORT,
+            },
+          ],
+        },
+        reporters: 'cli',
+      },
+      (err: Error | null, summary: NewmanRunSummary): void => {
+        const finalErr = err || summary.error;
+        if (finalErr) {
+          reject(finalErr);
+          return;
+        }
+        if (summary.run.failures && summary.run.failures.length > 0) {
+          reject(new Error('Postman tests failed'));
+          return;
+        }
+        resolve();
+      },
+    );
+  });
+
+const start = async (): Promise<void> => {
+  const download = await StartDownloadService();
+  const [gatewayServer, gateway] = await StartGateway();
+  for (let i = 0; i < maxRetries; i += 1) {
+    try {
+      await sleep(100);
+      // wait until the gateway has loaded the service definitions
+      await gateway.serviceHealthCheck();
+      // eslint-disable-next-line no-empty
+    } catch (err) {}
+  }
+  await runTests();
+  await Promise.all([download.stop(), gatewayServer.stop()]);
+};
+
+start().catch(error => {
+  // eslint-disable-next-line no-console
+  console.error('💥  Failed to run tests', error);
+  process.exit(1);
+});
