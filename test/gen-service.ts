0cb47c79513dd16139986c92848e8506ed45592c
Guilherme Iscaro <cabelitostos@gmail.com>
2022-04-18 13:55:13 -0300

    feat: update to apollo federation v2

--- test/gen-service.ts
+++ test/gen-service.ts
@@ -1,5 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server-express';
-import { buildSubgraphSchema } from '@apollo/federation';
+import { buildSubgraphSchema } from '@apollo/subgraph';
 import {
   FileUpload,
   graphqlUploadExpress,
